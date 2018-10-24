3f932a988563d6f8ced252ac294e533f042ad52b
Daria Krashchenko <dashakrashenko@MacBook-Pro-3.local>
2018-10-25 00:07:18 +0300

    First Commit

---
+++ CryptolLib/Protector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Linq;
+namespace CryptolLib
+{
+    public class Protector
+    {
+        private static readonly byte[] salt = Encoding.Unicode.GetBytes("7BANANAS");
+        private static readonly int iterations = 2000;
+
+        public static string Encrypt(string plainText, string password)
+        {
+            byte[] plainBytes = Encoding.Unicode.GetBytes(plainText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password,salt,iterations);
+            aes.Key = pbkdf2.GetBytes(32);
+            aes.IV = pbkdf2.GetBytes(16);
+
+            var ms = new MemoryStream();
+            return "abc";
+        }
+        
+    }
+}
---
+++ Encry/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Linq;
+namespace Encry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] salt = Encoding.Unicode.GetBytes("7BANANAS");
+            foreach(var a in salt)
+            {
+                System.Console.WriteLine(a);
+            }
+        }
+    }
+}
